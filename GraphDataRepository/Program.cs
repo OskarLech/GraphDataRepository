f36eb7f58ef650a41b53d65b27fe3475e9f3f4b0
Oskar Lech <oskar.lech1@wp.pl>
2017-04-07 01:00:17 +0200

    sync

--- GraphDataRepository/Program.cs
+++ GraphDataRepository/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using BrightstarDB.Client;
+using VDS.RDF;
+using Vocab;
 
 namespace GraphDataRepository
 {
@@ -10,6 +10,36 @@ namespace GraphDataRepository
     {
         static void Main(string[] args)
         {
+            var rdfsLabel = Foaf.geekcode;
+            var g = new Graph();
+
+            var context = BrightstarService.GetDataObjectContext("Type=rest;endpoint=http://localhost:8090/brightstar;");
+            {
+                context.CreateStore("MyStore");
+            }
+
+            if (context.DoesStoreExist("MyStore"))
+            {
+                return;
+            }
+
+            var store = context.OpenStore("MyStore");
+            var fred = store.MakeDataObject("http://example.org/people/fred");            var name = store.MakeDataObject("http://xmlns.com/foaf/0.1/name");
+            fred.SetProperty(name, "Fred Evans");
+
+            var x = fred.GetPropertyTypes();
+
+            Console.WriteLine("Properties of Fred:");
+            foreach (var propertyType in fred.GetPropertyTypes())
+            {
+                Console.WriteLine("\t" + propertyType.Identity + ":");
+                foreach (var propertyValue in fred.GetPropertyValues(propertyType))
+                {
+                    Console.WriteLine("\t\t" + propertyValue);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
