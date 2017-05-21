c3e68f6c02825d3797845d8cd2fe1374dd2a961d
Oskar Lech <oskar.lech1@wp.pl>
2017-05-21 23:17:40 +0200

    Interface for quality checks and QualityCheckReport class

---
+++ GraphDataRepository/QualityChecks/IQualityCheck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphDataRepository.QualityChecks
+{
+    public interface IQualityCheck
+    {
+        QualityCheckReport CheckGraphs(IEnumerable<Uri> graphs, IEnumerable<string> parameters = null);
+        QualityCheckReport CheckData(IEnumerable<string> triples, IEnumerable<Uri> graphs = null, IEnumerable<string> parameters = null); 
+        void FixErrors(QualityCheckReport checkReport, IEnumerable<int> errorsToFix);
+    }
+}
---
+++ GraphDataRepository/QualityChecks/QualityCheckReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphDataRepository.QualityChecks
+{
+    /// <summary>
+    /// Indicates if quality check passed and lists errors
+    /// </summary>
+    public class QualityCheckReport
+    {
+        public bool QualityCheckPassed { get; set; } = true;
+
+        //Key: ID, value: graph URI or triple, error message and flag indicating if it's possible to fix the error automatically
+        public readonly Dictionary<int, IEnumerable<Tuple<string, string, bool>>> ErrorsById = new Dictionary<int, IEnumerable<Tuple<string, string, bool>>>();
+    }
+}
