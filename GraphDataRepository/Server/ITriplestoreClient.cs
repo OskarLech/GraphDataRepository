4e590071e5a43d3594d19e3992897682ba31428b
Oskar Lech <oskar.lech1@wp.pl>
2017-06-20 20:39:38 +0200

    Removed IDisposable from ITriplestoreClient.cs

--- GraphDataRepository/Server/ITriplestoreClient.cs
+++ GraphDataRepository/Server/ITriplestoreClient.cs
@@ -9,7 +9,7 @@ namespace GraphDataRepository.Server
     /// <summary>
     /// Generic interface for basic CRUD operations any triplestore should provide
     /// </summary>
-    public interface ITriplestoreClient : IDisposable
+    public interface ITriplestoreClient
     {
         Task<bool> CreateDataset(string name);
         Task<bool> DeleteDataset(string name);
