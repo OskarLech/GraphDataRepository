f05a3520383cfb5cfde5583736fe3531e294e6a2
OskarLech <oskar.lech@oke.pl>
2017-06-25 20:41:03 +0200

    sync

---
+++ GraphDataRepository/QualityGrapher/ViewModels/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PropertyChanged;
+using QualityGrapher.Annotations;
+
+namespace QualityGrapher.ViewModels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
