4146713e3d7149149825fa4b73c01afac75bf6a1
Oskar Lech <oskar.lech1@wp.pl>
2017-07-05 00:56:17 +0200

    sync

--- Libraries/QualityChecks/SupportedQualityChecks.cs
+++ Libraries/QualityChecks/SupportedQualityChecks.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 
 namespace Libraries.QualityChecks
 {
-    public class SupportedQualityChecks
+    public class QualityChecksData
     {
-        public static IEnumerable<(string name, Type qualityCheckClass)> QualityChecksList = new List<(string, Type)>
+        public const string WholeDatasetSubject = "WholeDataset";
+        public static readonly Uri MetadataGraphUri = new Uri("resource://metadata");
+
+        public static IEnumerable<(string name, Type qualityCheckClass)> SupportedQualityChecks = new List<(string, Type)>
         {
             ("KnowledgeBase check", typeof(KnowledgeBaseCheck.KnowledgeBaseCheck)),
             ("Vocabulary check", typeof(VocabularyCheck.VocabularyCheck))
